7583fe90a35bc9d01fb9c635c99b2bbfd6dd1f02
ehuang535 <elaine.zy.huang@outlook.com>
2026-02-09 20:30:43 +1100

    config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
+  basePath: '/home/user/cserevue-website',
+  assetPrefix: '/home/user/cserevue-website/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
